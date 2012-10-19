77ec43760d663253d5a0d05c17e45d90fbe6bab2
Thompson Edolo <verygreenboi@live.com>
2012-10-19 02:25:53 +0100

    More updates

--- sql_grand_20120728.sql
+++ sql_grand_20120728.sql
@@ -1,118 +1,71 @@
--- MySQL dump 10.13  Distrib 5.5.9, for Win32 (x86)
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1    Database: grand
--- ------------------------------------------------------
--- Server version	5.5.24
+-- Host: localhost
+-- Generation Time: Oct 19, 2012 at 01:20 AM
+-- Server version: 5.5.24-log
+-- PHP Version: 5.4.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Current Database: `grand`
+-- Database: `grand`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `grand` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `grand`;
+-- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `businesses`
 --
 
-DROP TABLE IF EXISTS `categories`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categories` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` longtext NOT NULL,
-  `createdat` datetime DEFAULT NULL,
-  `updatedat` datetime DEFAULT NULL,
-  `deletedat` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categories`
---
-
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orders` (
+DROP TABLE IF EXISTS `businesses`;
+CREATE TABLE IF NOT EXISTS `businesses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `createdat` datetime DEFAULT NULL,
-  `updatedat` datetime DEFAULT NULL,
-  `deletedat` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `products`
---
-
-DROP TABLE IF EXISTS `products`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `description` longtext NOT NULL,
-  `categoryid` int(11) NOT NULL,
-  `stock` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
+  `userid` int(11) NOT NULL COMMENT 'created by',
+  `ceoid` int(11) NOT NULL COMMENT 'current ceo',
+  `catid` int(11) NOT NULL COMMENT 'Category id',
+  `name` text NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `updatedAt` datetime DEFAULT NULL,
   `deletedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='TODO: Add description column.' AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `products`
+-- Table structure for table `profiles`
 --
 
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE IF NOT EXISTS `profiles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `photourl` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `about` varchar(255) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  `deletedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `roles`
 --
 
 DROP TABLE IF EXISTS `roles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roles` (
+CREATE TABLE IF NOT EXISTS `roles` (
   `ID` int(11) NOT NULL,
   `role` varchar(45) NOT NULL,
   `createdat` datetime DEFAULT NULL,
@@ -120,60 +73,36 @@ CREATE TABLE `roles` (
   `deletedat` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `roles`
---
-
-LOCK TABLES `roles` WRITE;
-/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
-/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `role` int(10) NOT NULL,
   `firstname` varchar(45) NOT NULL,
   `lastname` varchar(45) NOT NULL,
   `email` varchar(127) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `photourl` varchar(255) DEFAULT NULL,
-  `address` varchar(255) DEFAULT NULL,
   `sex` varchar(10) NOT NULL,
   `phone` varchar(25) DEFAULT NULL,
   `urlid` varchar(45) DEFAULT NULL,
+  `salt` varchar(255) NOT NULL,
+  `passwordresettoken` varchar(255) DEFAULT NULL,
+  `passwordresetat` datetime DEFAULT NULL,
+  `confirmed` enum('1','0') NOT NULL,
+  `emailconfirmationtoken` varchar(255) DEFAULT NULL,
   `createdat` datetime DEFAULT NULL,
   `updatedat` datetime DEFAULT NULL,
   `deleteat` datetime DEFAULT NULL,
-  PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,3,'Thompson','Edolo','verygreenboi@live.com','06824988db540a8edb2d5b81ca076a7ce6ea8de0',NULL,NULL,'M',NULL,NULL,'2012-07-13 11:05:04','2012-07-13 11:05:04',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2012-07-28  8:53:35
--- views/partials/_gallery.cfm
+++ views/partials/_gallery.cfm
@@ -7,6 +7,11 @@
 	      	<li><img src="images/slide3.jpg" alt=""></li>
 	        <li><img src="images/slide2.jpg" alt=""></li>
 	        <li><img src="images/slide5.jpg" alt=""></li>
+            <li><img src="images/slide6.jpg" alt=""></li>
+            <li><img src="images/slide7.jpg" alt=""></li>
+            <li><img src="images/slide8.jpg" alt=""></li>
+            <li><img src="images/slide9.jpg" alt=""></li>
+            <li><img src="images/slide10.jpg" alt=""></li>
 	        <li><img src="images/slide1.jpg" alt=""></li>
 	        <li><img src="images/slide4.jpg" alt=""></li>
 	      </ul>
